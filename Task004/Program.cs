2d0b3fa33fc84776601a83095be73c30eb9d36f4
andreynikolaevtyumen <andreynikolaev66@gmail.com>
2022-08-09 18:48:26 +0500

    Initial commit

---
+++ Task004/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.WriteLine("Введите первое число из трех: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число из трех: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число из трех: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Максимальное число из трех введенных чисел: ");
+if (number1 >= number2 && number1>=number3)
+{
+   Console.WriteLine(number1);
+}
+else if (number2>number1 && number2>number3)
+{
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.WriteLine(number3);
+}
---
+++ Task008/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Все четные числа от 1 до числа N: ");
+for (int i = 2; i < n; i=i+2)
+{
+    Console.WriteLine(i);
+}
