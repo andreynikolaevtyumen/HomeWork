518c907324796cec3b50f417527a204e733579a4
andreynikolaevtyumen <andreynikolaev66@gmail.com>
2022-09-02 19:08:40 +0500

    Решение домашнего задания к уроку 6

---
+++ Task041/Program.cs
@@ -0,0 +1,33 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// Пользователь устанавливает размер массива.
+Console.Write($"Введи размер массива (число М): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+
+// Метод для заполнения массива.
+void FillArray(int m){
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введи {i+1}-е число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+// Метод для определения положительных чисел и их количества.
+int PositiveNumber(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if(array[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+FillArray(m);
+
+Console.WriteLine($"Массив содержит элементов больше 0: {PositiveNumber(array)}");
---
+++ Task043/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[,] coefficient = new double[2, 2];
+double[] crossPoint = new double[2];
+
+// Метод для ввода коэффициентов.
+void InpurCoeff()
+{
+  for (int i = 0; i < coefficient.GetLength(0); i++)
+  {
+    Console.Write($"Введите коэффициенты {i+1}-го уравнения (y = k * x + b):\n");
+    for (int j = 0; j < coefficient.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Введите коэффициент k: ");
+      else Console.Write($"Введите коэффициент b: ");
+      coefficient[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+// Решение уравнения.
+double[] solution(double[,] coefficient)
+{
+  crossPoint[0] = (coefficient[1,1] - coefficient[0,1]) / (coefficient[0,0] - coefficient[1,0]);
+  crossPoint[1] = crossPoint[0] * coefficient[0,0] + coefficient[0,1];
+  return crossPoint;
+}
+
+// Метод для нахождения точки пересечения.
+void OutputResponse(double[,] coefficient)
+{
+  if (coefficient[0,0] == coefficient[1,0] && coefficient[0,1] == coefficient[1,1]) 
+  {
+    Console.Write($"\nПрямые совпадают");
+  }
+  else if (coefficient[0,0] == coefficient[1,0] && coefficient[0,1] != coefficient[1,1]) 
+  {
+    Console.Write($"\nПрямые параллельны");
+  }
+  else 
+  {
+    solution(coefficient);
+    Console.Write($"\nТочка пересечения прямых: ({crossPoint[0]}, {crossPoint[1]})");
+  }
+}
+
+InpurCoeff();
+OutputResponse(coefficient);
